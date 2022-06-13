90689e69384d4cea1f6b59ae25d6dbe7d56ec80a
Samuel Levy <samulevy@gmail.com>
2022-06-13 08:12:04 -0300

    Add migration configurations

--- src/main/g8/config/config.exs
+++ src/main/g8/config/config.exs
@@ -9,7 +9,15 @@ import Config
 
 $if(include_database.truthy)$
 config :$name$,
-  ecto_repos: [$name;format="word-space,Camel"$.Repo]
+  ecto_repos: [$name;format="word-space,Camel"$.Repo],
+  generators: [binary_id: true]
+$endif$
+
+$if(include_database.truthy)$
+config :$name$, $name;format="word-space,Camel"$.Repo
+  migration_primary_key: [type: :uuid],
+  migration_foreign_key: [type: :uuid],
+  migration_timestamps: [type: :utc_datetime_usec]
 $endif$
 
 # Configures the endpoint
